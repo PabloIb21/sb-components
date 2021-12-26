047ea3175c03d2c868d406883f8470ebe5098324
Pablo Ibarra <pabloo_ibarra98@hotmail.com>
2021-12-25 22:18:18 -0600

    feat: add semantic-versioning

---
+++ dist/components/MyLabel.d.ts
@@ -0,0 +1,25 @@
+/// <reference types="react" />
+import './mylabel.css';
+export interface MyLabelProps {
+    /**
+    * Este es el mensaje a mostrar en la etiqueta
+    */
+    label: string;
+    /**
+    * Este es el tamaño de la etiqueta
+    */
+    size: 'normal' | 'h1' | 'h2' | 'h3';
+    /**
+    * Si quiere todo capitalizado
+    */
+    allCaps?: boolean;
+    /**
+    * Colores del botón
+    */
+    color?: 'primary' | 'secondary' | 'tertiary';
+    /**
+    * Color personalizado de la fuente
+    */
+    fontColor?: string;
+}
+export declare const MyLabel: ({ allCaps, label, color, size, fontColor, }: MyLabelProps) => JSX.Element;
---
+++ dist/stories/components/MyLabel.stories.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="react" />
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+declare const _default: ComponentMeta<({ allCaps, label, color, size, fontColor, }: import("../../components/MyLabel").MyLabelProps) => JSX.Element>;
+export default _default;
+export declare const Basic: ComponentStory<({ allCaps, label, color, size, fontColor, }: import("../../components/MyLabel").MyLabelProps) => JSX.Element>;
+export declare const AllCaps: ComponentStory<({ allCaps, label, color, size, fontColor, }: import("../../components/MyLabel").MyLabelProps) => JSX.Element>;
+export declare const Secondary: ComponentStory<({ allCaps, label, color, size, fontColor, }: import("../../components/MyLabel").MyLabelProps) => JSX.Element>;
+export declare const Tertiary: ComponentStory<({ allCaps, label, color, size, fontColor, }: import("../../components/MyLabel").MyLabelProps) => JSX.Element>;
+export declare const CustomFontColor: ComponentStory<({ allCaps, label, color, size, fontColor, }: import("../../components/MyLabel").MyLabelProps) => JSX.Element>;
